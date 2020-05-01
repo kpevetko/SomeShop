0db3c9a21b15e430f22ef816b7f5faad379116c7
kpevetko <lexux4@yandex.ru>
2020-05-01 11:20:44 +0700

    Future 02: add MORE logging for methods, add simple tests, fix some problem with HTML page and SQL query

--- popra/src/main/resources/forDB/AddNewSomeThingInDB.sql
+++ popra/src/main/resources/forDB/AddNewSomeThingInDB.sql
@@ -2,6 +2,9 @@ insert into products (name,description,cost,number) values ('пища','похо
 insert into products (name,description,cost,number) values ('яблоко','все еще похоже на еду',5,200);
 insert into products (name,description,cost,number) values ('велосипед','возможно, на этом можно прокатиться',300,20);
 insert into products (name,description,cost,number) values ('вилка','этим можно есть',20,100);
+insert into products (name,description,cost,number) values ('ложка','этим можно есть',20,100);
+insert into products (name,description,cost,number) values ('пова','это же человек, лучше его не есть',20,100);
+insert into products (name,description,cost,number) values ('трактор','на этом можно уехать...',1000,5);
 insert into users (username, password, enabled) values ('admin','admin',true);
 insert into users (username, password, enabled) values ('user','user',true);
 insert into authorities (username, authority) values ('admin','ROLE_ADMIN');
