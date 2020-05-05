c7fbfcb0eb212b875d902500232a42f59a659528
kpevetko <lexux4@yandex.ru>
2020-05-05 19:23:50 +0700

    Bugfix 01: fix some problem with HTML pages and services

--- popra/src/main/java/com/example/popra/service/UserServiceImpl.java
+++ popra/src/main/java/com/example/popra/service/UserServiceImpl.java
@@ -8,7 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+
 @Transactional
 @Service
 public class UserServiceImpl implements UserService {
@@ -22,32 +24,31 @@ public class UserServiceImpl implements UserService {
         this.authoritiesRepository = authoritiesRepository;
     }
 
-
-    //стоит синхронизация, предполагается что это поможет только одному потоку блокировать доступ к бд
-    //тем самым соблюдая очередь на создание (избежание коллизий)
+    //ранее было использование синхронайзе блока, но есть ведь у спринга такая замечательная транзакция
+    //установлено именно REPEATABLE_READ, поскольку по сравнению с Serializable, но у второй выше нагрузка, а первая
+    //хоть и подвержена фантомному чтению, но поскольку практически нет выборки - фантомное чтение фактически невозможно
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
     @Override
-    public  boolean createNewUser(String name, String pass) {
+    public boolean createNewUser(String name, String pass) {
         logger.info(String.format("Создания нового пользователя [логин: %s пароль: %s] (начало)", name, pass));
         try {
-            synchronized(userRepository){
-                if (!validate(name)) {
-                    logger.info(String.format("Создания нового пользователя [логин: %s пароль: %s] (неудачно, " +
-                            "пользователь с таким именем уже существует)", name, pass));
-                    return false;
-                }
-                User user = new User();
-                user.setUsername(name);
-                user.setPassword(pass);
-                user.setEnabled(true);
-                Authority authority = new Authority();
-                authority.setUsername(name);
-                authority.setAuthority("ROLE_USER");
-                authoritiesRepository.save(authority);
-                userRepository.save(user);
-
-                logger.info(String.format("Создания нового пользователя [логин: %s пароль: %s] (успешно, " +
-                        "пользователь создан)", name, pass));
+            if (!validate(name)) {
+                logger.info(String.format("Создания нового пользователя [логин: %s пароль: %s] (неудачно, " +
+                        "пользователь с таким именем уже существует)", name, pass));
+                return false;
             }
+            User user = new User();
+            user.setUsername(name);
+            user.setPassword(pass);
+            user.setEnabled(true);
+            Authority authority = new Authority();
+            authority.setUsername(name);
+            authority.setAuthority("ROLE_USER");
+            authoritiesRepository.save(authority);
+            userRepository.save(user);
+
+            logger.info(String.format("Создания нового пользователя [логин: %s пароль: %s] (успешно, " +
+                    "пользователь создан)", name, pass));
 
         } catch (Exception e) {
             logger.error(String.format("Создания нового пользователя [логин: %s пароль: %s] (ошибка: " +
@@ -57,9 +58,10 @@ public class UserServiceImpl implements UserService {
 
         return true;
     }
-    @Transactional(readOnly = true)
+
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED, readOnly = true)
     @Override
-    public  boolean validate(String name) {
+    public boolean validate(String name) {
         User user = userRepository.findByUsername(name);
         return user == null;
     }
